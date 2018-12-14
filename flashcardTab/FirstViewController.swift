f1ca475876440097f2b95886517a310dc3e97573
mac <mac@macs-imac.local>
2018-12-14 19:26:42 +0800

    Initial Commit

---
+++ flashcardTab/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  flashcardTab
+//
+//  Created by mac on 2018/12/14.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ flashcardTab/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  flashcardTab
+//
+//  Created by mac on 2018/12/14.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
