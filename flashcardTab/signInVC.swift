dc0682988cc90f2c4e7e43d93cea0b883cf89fa3
mac <mac@macs-imac.local>
2018-12-20 23:48:49 +0800

    fb user info added

--- flashcardTab/signInVC.swift
+++ flashcardTab/signInVC.swift
@@ -12,40 +12,71 @@ import FBSDKLoginKit
 
 
 
+
+
 class signInVC: UIViewController , FBSDKLoginButtonDelegate {
    
+  
     
-
     
    
     
+
+ 
+   
+        
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        let loginButton = FBSDKLoginButton()
+        
+        loginButton.readPermissions = ["public_profile", "email"]
+        view.addSubview(loginButton)
+        loginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
+        loginButton.delegate = self
 
-       let fbLoginBTN = FBSDKLoginButton()
-        view.addSubview(fbLoginBTN)
-        fbLoginBTN.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
         
-        fbLoginBTN.delegate = self
-        
-        
+       
     }
+    
+
+    
+
+    
+  
+    
+    
+    
     
     func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
         print("logget out")
         
         
     }
-    
-    
-    
+
+
     func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
-        if error != nil {
-            print("success logged in")
-        }
         
-    }
+        if error != nil {
+print("cant log in")
+            return
+        }else {
+            FBSDKGraphRequest(graphPath: "/me", parameters: ["fields":"id, name, email"])?.start{ (connection, result, error) in
+                if error != nil{
+                    print("error cant reach info " , error!)
+                }
+                
+                print(result!)
+                
+                
+            }
+            
+            
+            
+        }
     
   
   
+}
 }
